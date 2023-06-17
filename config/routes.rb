d7f2fa46559c3b9134d0178d4c114a6806b66ed1
EC2 Default User <ec2-user@ip-172-31-29-80.ap-northeast-1.compute.internal>
2023-06-17 05:03:56 +0000

    [Add]課題2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'homes/top'
+  resources :lists
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
